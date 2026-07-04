45d04f02eb15d373ea29189107a838b6ef892c2b
aanuoluwapooo <aanuoluwapoolurin@yahoo.com>
2026-07-04 09:17:33 +0100

    Add query for transactions with null customer IDs
    Added a query to find transactions with missing customer IDs.

--- project-01-banking-etl/sql/clean_transactions.sql
+++ project-01-banking-etl/sql/clean_transactions.sql
@@ -4,3 +4,8 @@ SELECT
 FROM bank_transactions
 GROUP BY Transaction_ID
 HAVING COUNT(*) > 1;
+-- Find transactions with missing customer IDs
+
+SELECT *
+FROM bank_transactions
+WHERE Customer_ID IS NULL;
